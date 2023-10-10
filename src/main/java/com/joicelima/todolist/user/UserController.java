2c34e8ac9df3235e8ce4cbef7a0d3d930668f23f
JoiceStefany <steffanyjoice006@gmail.com>
2023-10-10 20:45:00 -0300

    Integrando ao Banco de Dados - h2 DataBase

--- src/main/java/com/joicelima/todolist/user/UserController.java
+++ src/main/java/com/joicelima/todolist/user/UserController.java
@@ -1,17 +1,33 @@
 package com.joicelima.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create (@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
-    }
+    public ResponseEntity create (@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null ){
+            System.out.println("Usuário já existe.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("usuario ja existe");
+        }
+
+      var userCreated = this.userRepository.save(userModel);
+      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     
+    }
 }
